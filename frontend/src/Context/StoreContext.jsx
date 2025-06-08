f3e0dd63d7a20230e01f3240a4787be8e7aa5029
sulyman560 <sulyman560@gmail.com>
2025-06-08 23:31:13 +0600

    Update StoreContext.jsx

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null)
 
 const StoreContextProvider = (props) => {
     const [cartItem, setCartItem] = useState({});
-    const url = "http://localhost:5000"
+    const url = "https://food-del-backend-jcpg.onrender.com"
     const [token, setToken] = useState("")
     const [food_list, setFoodList] = useState([])
     const fetchFoodList = async () => {
